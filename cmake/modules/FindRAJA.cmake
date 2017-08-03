70ead33f53d4f18838ff24e935e7779868c26c68
David Truby <david.truby@ibm.com>
2017-08-03 15:09:21 -0400

    Existing RAJA omp implementation.

---
+++ cmake/modules/FindRAJA.cmake
@@ -0,0 +1,23 @@
+# - Try to find RAJA
+
+find_package(OpenMP REQUIRED)
+
+set(RAJA_CXX_FEATURES cxx_auto_type cxx_constexpr cxx_generic_lambdas)
+
+find_path(RAJA_INCLUDE_DIR RAJA/RAJA.hpp
+  HINTS ${RAJA_ROOT}/include $ENV{RAJA_ROOT}/include)
+find_library(RAJA_LIBRARY NAMES RAJA libRAJA
+  HINTS ${RAJA_ROOT}/lib)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RAJA DEFAULT_MSG RAJA_LIBRARY RAJA_INCLUDE_DIR)
+mark_as_advanced(RAJA_LIBRARY RAJA_INCLUDE_DIR)
+
+set(RAJA_LIBRARIES ${RAJA_LIBRARY})
+set(RAJA_INCLUDE_DIRS ${RAJA_INCLUDE_DIR})
+
+add_library(RAJA INTERFACE)
+target_include_directories(RAJA INTERFACE ${RAJA_INCLUDE_DIRS})
+target_link_libraries(RAJA INTERFACE ${RAJA_LIBRARIES})
+target_compile_features(RAJA INTERFACE ${RAJA_CXX_FEATURES})
+#target_compile_options(RAJA INTERFACE ${OpenMP_CXX_FLAGS})
